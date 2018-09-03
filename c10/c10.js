8703eb7b30d74eb61944c87e5e07f8bdd6c00bae
zhao9426 <2395355749@qq.com>
2018-09-03 19:17:05 +0800

    one

---
+++ c10/c10.js
@@ -0,0 +1,9 @@
+var subtotal=(13+1)*5;
+var shipping=0.5*(13+1);
+var total=subtotal+shipping;
+var elsub=document.getElementById('subtotal');
+elsub.textContent=subtotal;
+var elship=document.getElementById('shipping');
+elship.textContent=shipping;
+var eltotal=document.getElementById('total');
+eltotal.textContent=total;
